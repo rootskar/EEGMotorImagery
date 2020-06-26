55af60af07b9e97568d1544022db42c861c0188e
Karel Roots <33195807+rootskar@users.noreply.github.com>
2020-06-26 12:28:41 +0300

    Updated code

--- run_experiments.py
+++ run_experiments.py
@@ -14,7 +14,6 @@ from mcnemar import mcnemar_test
 from tensorflow.keras import backend as K
 from tensorflow.keras.utils import to_categorical
 from training_testing import run_experiment
-from transfer_learning import run_tl_experiment
 from run_type import RunType
 
 """
@@ -76,45 +75,8 @@ else:
     X = X.reshape(X.shape[0], 1, X.shape[1], X.shape[2])
 y = to_categorical(y, nb_classes)
 
-print("103-subject X shape: {}".format(X.shape))
-print("103-subject y shape: {}".format(y.shape))
-
-# Extracting first 6 subject data
-trials_for_6_subjects = 6 * trials_per_subject
-X_6 = X[:trials_for_6_subjects]
-y_6 = y[:trials_for_6_subjects]
-
-print("6-subject X shape: {}".format(X_6.shape))
-print("6-subject y shape: {}".format(y_6.shape))
-
-# Extracting first 20 subject data
-trials_for_20_subjects = 20 * trials_per_subject
-X_20 = X[:trials_for_20_subjects]
-y_20 = y[:trials_for_20_subjects]
-
-print("20-subject X shape: {}".format(X_20.shape))
-print("20-subject y shape: {}".format(y_20.shape))
-
-# Extracting 100-subject data
-trials_for_100_subjects = subj_for_training * trials_per_subject
-X_100 = X[:trials_for_100_subjects]
-y_100 = y[:trials_for_100_subjects]
-
-print("100-subject X shape: {}".format(X_100.shape))
-print("100-subject y shape: {}".format(y_100.shape))
-
-# Extracting data of last 3 subjects for transfer learning
-X_tl = []
-y_tl = []
-start_idx = trials_for_100_subjects
-for i in range(subj_for_transfer_learning):
-    end_idx = start_idx + trials_per_subject
-    X_tl.append(X[start_idx:end_idx])
-    y_tl.append(y[start_idx:end_idx])
-    start_idx = end_idx
-
-print("TL 1 subject X_train shape: {}".format(X_tl[0].shape))
-print("TL 1 subject y_train shape: {}".format(y_tl[0].shape))
+print("X shape: {}".format(X.shape))
+print("y shape: {}".format(y.shape))
 
 # Make directories for model binaries
 DIR = ['./model', './history']
@@ -125,27 +87,10 @@ for directory in DIR:
 # Perform experiments
 experiments = []
 
-# test models with subjects 1-6
-experiment_6sub = Experiment(trial_type, '6sub', get_models(trial_type, nb_classes, samples, use_cpu), nr_of_epochs,
-                             0.125, 0.2)
-experiments.append(run_experiment(X_6, y_6, experiment_6sub))
-
-# for imagined tasks test models with subjects 1-20
-if trial_type == RunType.Imagined:
-    experiment_20sub = Experiment(trial_type, '20sub', get_models(trial_type, nb_classes, samples, use_cpu),
-                                  nr_of_epochs, 0.125, 0.2)
-    experiments.append(run_experiment(X_20, y_20, experiment_20sub))
-
 # test models with all subjects
 experiment_103sub = Experiment(trial_type, '103sub', get_models(trial_type, nb_classes, samples, use_cpu), nr_of_epochs,
                                0.125, 0.2)
 experiments.append(run_experiment(X, y, experiment_103sub))
-
-# test models with transfer learning
-experiment_tl = Experiment(trial_type, 'tl', get_models(trial_type, nb_classes, samples, use_cpu), nr_of_epochs, 0.1,
-                           0.5)
-experiments.extend(run_tl_experiment(experiment_tl, X_100, y_100, X_tl, y_tl, subj_for_training,
-                                     trial_type, nb_classes, samples, use_cpu=use_cpu, pre_train=True))
 
 # Calculate Mcnemar's test statistic and p-value for all experiments
 models = ['EEGNet', 'ShallowConvNet', 'DeepConvNet']
